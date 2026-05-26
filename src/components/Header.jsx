0e9a83514d40acbd5679cb9914ba5ba11ed07fc8
markxvych <arsenmarcovych666@gmail.com>
2026-05-26 23:12:55 +0300

    small changes in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,30 @@ function Header() {
   return (
     <header className="d-flex justify-between align-center p-40">
       <div className="d-flex align-center">
-        <img src="/img/headerLogo.png" width={40} height={40} />
-
+        <img
+          className="mr-15"
+          src="/img/headerLogo.png"
+          alt="Logo"
+          width={40}
+          height={40}
+        />
         <div>
           <h3 className="text-uppercase">React Sneakers</h3>
           <p className="opacity-5">Магазин найкращих кросівок</p>
         </div>
       </div>
-      <ul className="d-flex">
-        <li className="mr-30">
-          <img src="/icons/trash.svg" width={18} height={18} />
+      <ul>
+        <li>
+          <img src="/icons/trash.svg" alt="Trash" width={18} height={18} />
           <span>1205 ₴</span>
         </li>
         <li>
-          <img src="/icons/user.svg" width={18} height={18} />
+          <img src="/icons/heart.svg" alt="Favorite" width={18} height={18} />
+          <span>Закладки</span>
+        </li>
+        <li>
+          <img src="/icons/user.svg" alt="User" width={18} height={18} />
+          <span>Профіль</span>
         </li>
       </ul>
     </header>
